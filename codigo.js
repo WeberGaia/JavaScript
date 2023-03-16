1b720b06949f843814805562439d633ab605452e
Weber Gaia <webergaia@id.uff.br>
2023-03-15 22:28:52 -0300

    JavaScript Course

---
+++ codigo.js
@@ -0,0 +1,18 @@
+//Tipo Numérico
+const meuNumero = 3;
+const num1 = 2;
+const total = meuNumero * num1;
+
+//Tipo String
+const texto1 = 'Olá, "Weber"';
+const nome = "Weber ";
+const sobre = "Gaia"
+const nome_total = nome + sobre;
+//console.log(nome_total);
+
+//Tipo Booleano
+const num11 = 10;
+const num22 = 10;
+let cadastro = true;
+console.log(num11 === num22);       //Comparação
+
